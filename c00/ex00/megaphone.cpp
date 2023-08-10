b67a9f00ae14d8cd0320585e6c85effc2acc72c6
hamzatlh <htlhaouia@gmail.com>
2023-08-10 21:12:13 +0100

    fix" "

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:03:18 by htalhaou          #+#    #+#             */
-/*   Updated: 2023/08/06 17:58:04 by htalhaou         ###   ########.fr       */
+/*   Updated: 2023/08/09 19:00:41 by htalhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ int main(int argc, char **argv)
 {
 	int i;
 
-	i = 0;
+	i = 1;
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	while(argv[++i])
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while(argv[i])
 	{
 		for(int j = 0; argv[i][j]; j++)
 				std::cout << (char)std::toupper(argv[i][j]);
-		std::cout << " ";
+		if (argv[i + 1] != NULL)
+			std::cout << " ";
+		i++;
 	}
 	std::cout << std::endl;
 	return (0);
