4deccd943c9bbd78db2599d2ee941142e8b5559a
hamzatlh <htlhaouia@gmail.com>
2023-09-14 20:58:52 +0100

    more cpp

--- C06/ex00/ScalarConverter.cpp
+++ C06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 20:34:29 by htalhaou          #+#    #+#             */
-/*   Updated: 2023/09/14 16:27:33 by htalhaou         ###   ########.fr       */
+/*   Updated: 2023/09/14 20:58:37 by htalhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,13 +66,15 @@ static int isFloat(std::string str)
 			dot++;
 		if (dot > 1)
 			return (0);
-		if (!std::isdigit(str[i]) && str[i] != '.')
+		if (!std::isdigit(str[i]) && dot == 0)
 			return (0);
-		if (str[i] == 'f' && str[i + 1] != '\0')
+		if (str[i] == '.' && str[i] == 'f' && str[i + 1] != '\0')
 			return (0);
-		if (!std::isdigit(str[i]) && dot == 1 && str[i + 1] == 'f')
+		if (str[i] == '.' && str[i + 1] == 'f')
 			return (0);
-		if(str[i] == 'f' ||( dot == 1 && str[i + 1] == 'f' && str[i + 2] == '\0'))
+		// if (!std::isdigit(str[i]) && dot == 1 && str[i + 1] == 'f')
+		// 	return (0);
+		if(str[i] == 'f' && ( dot == 1 && str[i + 1] != 'f' && str[i + 2] == '\0'))
 			return (1);
 		i++;
 	}
@@ -87,15 +89,15 @@ static int isDouble(std::string str)
 		i++;
 	while (str[i])
 	{
-		if (str[i] != 'f' && !std::isdigit(str[i]) && str[i] != '.')
-			return (0);
-		if ( str[i] == 'f' && str[i + 1] != '\0')
-			return (0);
+		// if (str[i] != 'f' && !std::isdigit(str[i]) && str[i] != '.')
+		// 	return (0);
 		if (str[i] == '.')
 			dot++;
 		if (dot > 1)
 			return (0);
 		if (!std::isdigit(str[i]) && dot == 1 && str[i + 1] == '\0')
+			return (0);
+		if (str[i] == 'f' && str[i + 1] != '\0')
 			return (0);
 		i++;
 	}
@@ -104,16 +106,20 @@ static int isDouble(std::string str)
 
 const char* ScalarConverter::ImpossibleException::what() const throw()
 {
-	return "impossible";
+		return ("char : impossible\nint : impossible\nfloat : impossible\ndouble : impossible"); 
 }
 
 void ScalarConverter::convert(std::string str)
 {
 	std::cout << std::fixed << std::setprecision((str.length() - str.find('.') - 2));
+	char *end;
+	std::strtod(str.c_str(), &end);
 	if (isChar(str))
 	{
+		if (*end != '\0' && str.length() != 1)
+			throw ImpossibleException();
 		printf("char heere\n");
-	std::cout << std::fixed << std::setprecision((str.length() - str.find('.') - 1));
+		std::cout << std::fixed << std::setprecision((str.length() - str.find('.') - 1));
 		std::cout << "char: '" << str[0] << "'" << std::endl;
 		std::cout << "int: " << static_cast<int>(str[0]) << std::endl;
 		std::cout << "float: " << static_cast<double>(str[0]) << "f" << std::endl;
@@ -138,7 +144,7 @@ void ScalarConverter::convert(std::string str)
 			std::cout << "int : impossible" << std::endl;
 		else
 			std::cout << "int : " <<  static_cast<int>(i) << std::endl;
-		std::cout << "float : " << static_cast<double>(i) << "f" << std::endl;
+		std::cout << "float : " << static_cast<float>(i) << "f" << std::endl;
 		std::cout << "double: " << static_cast<double>(i) << std::endl;
 	}
 	else if (isFloat(str))
@@ -146,6 +152,9 @@ void ScalarConverter::convert(std::string str)
 		printf("float heere\n");
 		char *end;
 		double f = std::strtod(str.c_str(), &end);
+		printf("====> %f\n", f);
+		if (*end != '\0' && strlen(end) != 1)
+			throw ImpossibleException();
 		if (f < 32 || f > 126)
 		{
 			if (f > INT_MAX || f < INT_MIN || *end != '\0')
@@ -166,15 +175,23 @@ void ScalarConverter::convert(std::string str)
 	else if (isDouble(str))
 	{
 		printf("double heere\n");
+		std::cout << std::fixed << std::setprecision((str.length() - str.find('.') - 1));
 		char *end;
 		double d = std::strtod(str.c_str(), &end);
-		printf("====> %s\n", end);
+		if (*end != '\0' && str.length() != 1)
+			throw ImpossibleException();
+		if (str == "nan" || str == "nanf")
+			{
+				std::cout << "char: impossible" << std::endl;
+				std::cout << "int: impossible" << std::endl;
+			}
+		else
 		if (d < 32 || d > 126)
 		{
 			if (d > INT_MAX || d < INT_MIN || *end != '\0')
 				std::cout << "char : impossible" << std::endl;
 			else
-				std::cout << "char: Non displayable" << static_cast<char>(d) << "'" << std::endl;	
+				std::cout << "char: Non displayable" << static_cast<char>(d) << std::endl;	
 		}
 		else
 			std::cout << "char: '" << static_cast<char>(d) << "'" << std::endl;
@@ -183,15 +200,9 @@ void ScalarConverter::convert(std::string str)
 			std::cout << "impossible" << std::endl;
 		else
 			std::cout << static_cast<int>(d) << std::endl;
-		std::cout << "float: " << static_cast<double>(d) << "f" << std::endl;
+		std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
 		std::cout << "double: " << d << std::endl;
 	}
 	else
-	{
-		printf("impossible heere\n");
-		std::cout << "char : impossible" << std::endl;
-		std::cout << "int : impossible" << std::endl;
-		std::cout << "float : impossible" << std::endl;
-		std::cout << "double : impossible" << std::endl;
-	}
+		throw ImpossibleException();
 }
