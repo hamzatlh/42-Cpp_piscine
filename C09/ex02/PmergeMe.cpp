aeb978096dfa0cdcb57e12683457ba1830c743cf
hamzatlh <htlhaouia@gmail.com>
2023-10-19 09:35:00 +0100

    more cpp

--- C09/ex02/PmergeMe.cpp
+++ C09/ex02/PmergeMe.cpp
@@ -1,8 +1,10 @@
 
 #include "PmergeMe.hpp"
 
+static int comp=0;
 bool Compare(const std::deque<int>& d1, const std::deque<int>& d2)
 {
+    comp++;
     return (d1.back() < d2.back());
 }
 
@@ -18,7 +20,7 @@ void split_deque_into_pairs(std::deque<int>& d, size_t size)
             tmp.push_back(d[i]);
         d.erase(d.end() - (d.size() % (size * 2)), d.end());
         std::cout << std::endl;
-        std::cout << "==> tmp" << std::endl;
+        // std::cout << "==> tmp" << std::endl;
     }
     for (std::deque<int>::iterator it = tmp.begin(); it != tmp.end(); ++it)
         std::cout << *it << " ";
@@ -44,7 +46,10 @@ void split_deque_into_pairs(std::deque<int>& d, size_t size)
             for (size_t j = i + size; j < i + size * 2; j++)
                 second_deque.push_back(d[j]);
             if (first_deque.back() > second_deque.back())
+            {
+                comp++;
                 std::swap(first_deque, second_deque);
+            }
         }
         pairs.push_back(std::make_pair(first_deque, second_deque));
     }
@@ -58,20 +63,20 @@ void split_deque_into_pairs(std::deque<int>& d, size_t size)
         for (size_t j = 0; j < size; j++)
             d.push_back(second_subdeque[j]);
     }
-    std::cout << "==> pairs in " << size << std::endl;
-    for (std::deque<std::pair<std::deque<int>, std::deque<int> > >::iterator it = pairs.begin(); it != pairs.end(); ++it)
-    {
-        for (std::deque<int>::iterator it2 = it->first.begin(); it2 != it->first.end(); ++it2)
-            std::cout << *it2 << " ";
-        std::cout << " , ";
-        for (std::deque<int>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
-            std::cout << *it2 << " ";
-        std::cout << std::endl;
-    }
+    // std::cout << "==> pairs in " << size << std::endl;
+    // for (std::deque<std::pair<std::deque<int>, std::deque<int> > >::iterator it = pairs.begin(); it != pairs.end(); ++it)
+    // {
+    //     for (std::deque<int>::iterator it2 = it->first.begin(); it2 != it->first.end(); ++it2)
+    //         std::cout << *it2 << " ";
+    //     std::cout << " , ";
+    //     for (std::deque<int>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+    //         std::cout << *it2 << " ";
+    //     std::cout << std::endl;
+    // }
     split_deque_into_pairs(d, size * 2);
     pairs.clear();
     std::cout << std::endl;
-    std::cout << "size = " << size << std::endl;
+    // std::cout << "size = " << size << std::endl;
     std::deque<std::deque<int> > main_chain;
     std::deque<std::deque<int> > pend_chain;
     for (size_t i = 0; i < d.size(); i += size * 2)
@@ -120,22 +125,45 @@ void split_deque_into_pairs(std::deque<int>& d, size_t size)
         std::cout << " , ";
     }
     std::cout << std::endl;
-    std::cout << "===> pend_chain" << std::endl;
-    for (std::deque<std::deque<int> >::iterator it = pend_chain.begin(); it != pend_chain.end(); ++it)
-    {
-        for (std::deque<int>::iterator it2 = it->begin(); it2 != it->end(); ++it2)
-            std::cout << *it2 << " ";
-        std::cout << " , ";
-    }
-    std::cout << std::endl;
-    std::cout << "==> pairs in " << size << std::endl;
-    for (std::deque<std::pair<std::deque<int>, std::deque<int> > >::iterator it = pairs.begin(); it != pairs.end(); ++it)
-    {
-        for (std::deque<int>::iterator it2 = it->first.begin(); it2 != it->first.end(); ++it2)
-            std::cout << *it2 << " ";
-        std::cout << " , ";
-        for (std::deque<int>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
-            std::cout << *it2 << " ";
-        std::cout << std::endl;
-    }
+    // std::cout << "===> pend_chain" << std::endl;
+    // for (std::deque<std::deque<int> >::iterator it = pend_chain.begin(); it != pend_chain.end(); ++it)
+    // {
+    //     for (std::deque<int>::iterator it2 = it->begin(); it2 != it->end(); ++it2)
+    //         std::cout << *it2 << " ";
+    //     std::cout << " , ";
+    // }
+    // std::cout << std::endl;
+    // std::cout << "==> pairs in " << size << std::endl;
+    // for (std::deque<std::pair<std::deque<int>, std::deque<int> > >::iterator it = pairs.begin(); it != pairs.end(); ++it)
+    // {
+    //     for (std::deque<int>::iterator it2 = it->first.begin(); it2 != it->first.end(); ++it2)
+    //         std::cout << *it2 << " ";
+    //     std::cout << " , ";
+    //     for (std::deque<int>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+    //         std::cout << *it2 << " ";
+    //     std::cout << std::endl;
+    // }
+}
+
+
+int main (int ac, char **av)
+{
+	std::deque<int> d;
+	std::deque<std::pair<std::deque<int>, std::deque<int> > > pairs;
+
+	if (ac < 2)
+	{
+		std::cout << "Error : please give me a list of integers pleaaaseee" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+		d.push_back(atoi(av[i]));
+	std::cout << "==> d : ";
+	for (std::deque<int>::iterator it = d.begin(); it != d.end(); it++)
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	split_deque_into_pairs(d, 1);
+
+	std::cout << "comp :  " << comp << std::endl;
+	return (0);
 }
--- C09/ex02/main.cpp
+++ C09/ex02/main.cpp
@@ -6,28 +6,30 @@
 /*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 15:20:40 by htalhaou          #+#    #+#             */
-/*   Updated: 2023/10/16 13:10:50 by htalhaou         ###   ########.fr       */
+/*   Updated: 2023/10/19 08:55:23 by htalhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-int main (int ac, char **av)
-{
-	std::deque<int> d;
-	std::deque<std::pair<std::deque<int>, std::deque<int> > > pairs;
+// int main (int ac, char **av)
+// {
+// 	std::deque<int> d;
+// 	std::deque<std::pair<std::deque<int>, std::deque<int> > > pairs;
 
-	if (ac < 2)
-	{
-		std::cout << "Error : please give me a list of integers pleaaaseee" << std::endl;
-		return (0);
-	}
-	for (int i = 1; i < ac; i++)
-		d.push_back(atoi(av[i]));
-	std::cout << "==> d : ";
-	for (std::deque<int>::iterator it = d.begin(); it != d.end(); it++)
-		std::cout << *it << " ";
-	std::cout << std::endl;
-	split_deque_into_pairs(d, 1);
-	return (0);
-}
+// 	if (ac < 2)
+// 	{
+// 		std::cout << "Error : please give me a list of integers pleaaaseee" << std::endl;
+// 		return (0);
+// 	}
+// 	for (int i = 1; i < ac; i++)
+// 		d.push_back(atoi(av[i]));
+// 	std::cout << "==> d : ";
+// 	for (std::deque<int>::iterator it = d.begin(); it != d.end(); it++)
+// 		std::cout << *it << " ";
+// 	std::cout << std::endl;
+// 	split_deque_into_pairs(d, 1);
+
+// 	std::cout << "comp :  " << comp << std::endl;
+// 	return (0);
+// }
